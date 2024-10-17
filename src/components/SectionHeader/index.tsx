67b354e5b2de03d95bbb9f496a509c16f4af4889
blackcow1987 <blackcow@graylab.team>
2024-10-18 02:39:54 +0900

    changed SectionHeader and Wrapper to server component

--- src/components/SectionHeader/index.tsx
+++ src/components/SectionHeader/index.tsx
@@ -1,17 +1,16 @@
-"use client";
-
 import React from "react";
 
-import { styled } from "@mui/material";
-
-const SectionHeader = (props) => {
-  const { title, url, imgClass } = props;
-  const Header = styled("header")({
-    backgroundColor: "#FFF0DD",
-  });
-
+const SectionHeader = ({
+  title,
+  url,
+  imgClass,
+}: {
+  title: string;
+  url: string;
+  imgClass: string;
+}) => {
   return (
-    <Header className="h-[32rem] pt-[6.5rem] max-md:h-[18rem] max-md:pt-[6.2rem]">
+    <div className="h-[32rem] bg-[#FFF0DD] pt-[6.5rem] max-md:h-[18rem] max-md:pt-[6.2rem]">
       <div className="mx-auto flex h-full max-w-[1536px] items-center justify-between px-[10.4rem] max-md:px-[1.6rem]">
         <h1 className="mt-[-2rem] text-[56px] font-semibold leading-[1.2] text-stone-950 max-md:mt-0 max-md:text-[3.2rem]">
           {title}
@@ -21,7 +20,7 @@ const SectionHeader = (props) => {
           className={`max-h-full object-contain ${imgClass} max-md:max-h-[90%]`}
         />
       </div>
-    </Header>
+    </div>
   );
 };
 
--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,12 +1,8 @@
-"use client";
-
 import React from "react";
 
-const Wrapper = (props) => {
-  const { children } = props;
-
+const Wrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto w-full max-w-[153.6rem] px-[10.4rem] max-md:px-[1.6rem] ">
+    <div className="mx-auto w-full max-w-[153.6rem] px-[10.4rem] max-md:px-[1.6rem]">
       {children}
     </div>
   );

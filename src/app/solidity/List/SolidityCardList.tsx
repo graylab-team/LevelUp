97920b5dfaace73d414184524b19ed40ce1a4b06
blackcow1987 <blackcow@graylab.team>
2024-10-18 03:03:12 +0900

    changed List and SolidityCardList to server component

--- src/app/solidity/List/SolidityCardList.tsx
+++ src/app/solidity/List/SolidityCardList.tsx
@@ -1,86 +1,31 @@
-import { Box, Typography, styled } from "@mui/material";
-import { useRouter } from "next/navigation";
-import { sendGAEvent } from '@next/third-parties/google'
+import { Box } from "@mui/material";
+import Link from "next/link";
 
-const CardArticle = styled("article")(() => ({
-  padding: "2.4rem",
-  background: "#FFF0DD",
-  borderRadius: "2rem",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
-  cursor: "pointer",
-  ["& *"]: {
-    cursor: "pointer !important",
-  },
-}));
-
-const Title = styled(Typography)(() => ({
-  fontSize: "2rem",
-  lineHeight: "2.8rem",
-  marginBottom: "1.2rem",
-  fontWeight: 600,
-}));
-
-const Summary = styled(Typography)(() => ({
-  fontSize: "1.6rem",
-  lineHeight: "2.4rem",
-  marginBottom: "1.2rem",
-  color: " #5B5B5B",
-}));
-
-const LabelContainer = styled("div")(() => ({
-  display: "flex",
-  gap: "2rem",
-  marginTop: "1.6rem",
-}));
-
-const Label = styled(Typography)(() => ({
-  fontSize: "1.6rem",
-  background: "#FFDEB5",
-  borderRadius: "0.4rem",
-  color: "#84623A",
-  height: "auto",
-  padding: "0.4rem 1.6rem",
-  lineHeight: "normal",
-  fontWeight: "500",
-  textAlign: "center",
-  whiteSpace: "normal",
-  overflowWrap: "break-word", // Added break-word to prevent overflow
-}));
-
-const SolidityCardList = ({ content, key }) => {
-  const router = useRouter();
-
-  const handleClick = () => {
-    sendGAEvent('event', 'challengeClicked', { value: `Lesson ${content.lesson}: ${content.name}` })
-    if (content.url) {
-      window.open(content.url);
-    } else {
-      router.push(`${location.pathname}/${content.id}`);
-    }
-  };
-
+const SolidityCardList = ({ content }) => {
   return (
-    <CardArticle
-      key={key}
-      onClick={handleClick}
+    <Link
+      href={`/solidity/${content.id}`}
+      target={content?.url ? "_blank" : undefined}
+      className="flex cursor-pointer flex-col justify-between rounded-[2rem] bg-[#FFF0DD] p-6"
     >
-      <Box>
-        <Title>
-          {" "}
-          Lesson {content.lesson}: {content.name}{" "}
-        </Title>
-        <Summary> {content.summary} </Summary>
-      </Box>
-      {/* TODO: Update labels in future. Temporarily removed for MVP */}
-      {/* <LabelContainer>
+      <div>
+        <Box>
+          <div className="mb-[1.2rem] text-[2rem] font-semibold leading-[2.8rem]">
+            Lesson {content.lesson}: {content.name}
+          </div>
+          <div className="mb-3 text-[1.6rem] leading-[2.4rem] text-[#5B5B5B]">
+            {content.summary}
+          </div>
+        </Box>
+        {/* TODO: Update labels in future. Temporarily removed for MVP */}
+        {/* <LabelContainer>
         {content.labels?.map((label, index) => (
           <Label key={index}>{label}</Label>
         ))}
         {content.level ? <Label>Level {content.level}</Label> : null}
       </LabelContainer> */}
-    </CardArticle>
+      </div>
+    </Link>
   );
 };
 
